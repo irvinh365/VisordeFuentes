a1d6e7d2ea4d545f40817b7402d57b12e0133d70
Irving Hass <19470232@itcampeche.edu.mx>
2020-10-25 15:44:15 -0600

    Agrega el manejador de eventos en la aplicación.

--- VisorDeFuentes/MainWindow.xaml.cs
+++ VisorDeFuentes/MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace VisorDeFuentes
         {
             InitializeComponent();
         }
+
+        private void fuenteComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          if (fuenteComboBox.SelectedItem != null)
+            {
+                string fuente = (string)(fuenteComboBox.SelectedItem as ComboBoxItem).Content;
+                LeyendaTextblock.FontFamily = new FontFamily(familyName: fuente);
+            }
+        }
+
+        private void cursivaCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            LeyendaTextblock.FontStyle = FontStyles.Italic;
+        }
+
+        private void cursivaCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            LeyendaTextblock.FontStyle = FontStyles.Normal;
+        }
+
+        private void negritasCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            LeyendaTextblock.FontWeight = FontWeights.Bold;
+        }
+
+        private void negritasCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            LeyendaTextblock.FontWeight = FontWeights.Normal;
+        }
+
+        private void smallRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tamanioRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (smallRadioButton.IsChecked == true)
+            {
+                LeyendaTextblock.FontSize = 12;
+            }
+            else if (mediumRadioButton.IsChecked == true)
+            {
+                LeyendaTextblock.FontSize = 18;
+            }
+            else
+            {
+                LeyendaTextblock.FontSize = 24;
+            }
+        }
     }
 }
